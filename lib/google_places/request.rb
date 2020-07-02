d2f94e560d66d7828a508bce45a2bd3a21a0dc88
Diego del Valle <diego.valle@dixtra.co>
2020-07-02 16:02:37 -0300

    puts

--- lib/google_places/request.rb
+++ lib/google_places/request.rb
@@ -316,6 +316,7 @@ module GooglePlaces
       retry_options[:max]    ||= 0
       retry_options[:delay]  ||= 5
       retry_options[:status] = [retry_options[:status]] unless retry_options[:status].is_a?(Array)
+      puts options
       @response = self.class.get(url, :query => options, :follow_redirects => follow_redirects)
 
       return unless retry_options[:max] > 0 && retry_options[:status].include?(@response.parsed_response['status'])
@@ -346,6 +347,7 @@ module GooglePlaces
     end
 
     def execute
+      puts options
       @response = self.class.get(url, :query => options, :follow_redirects => follow_redirects)
     end
 
